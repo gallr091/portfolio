98c444b943fb07d08afe88d47b10d64b4df40e99
rice gallardo <gallr091@newschool.edu>
2025-06-03 14:29:38 -0400

    mobile nav overlay

--- script.js
+++ script.js
@@ -370,3 +370,9 @@ letters.forEach(letter => {
     });
 });
 
+//FUNCTION: NAV
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  const nav = document.getElementById('overlay-nav');
+  nav.classList.toggle('show');
+});
+
